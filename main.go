58202bc42b6fc32b710a9d1b8fc61e052cac97fb
asrieannisa <pratiwiasrieannisa@gmail.com>
2023-09-16 14:41:14 +0700

    update coding 16 sept 2023

--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"BasicTrade/database"
 	router "BasicTrade/routers"
-)
+	"os"
 
-var (
-	PORT = ":7070"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load variables from .env file
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
+
+	// Get the PORT value from the environment
+	PORT := os.Getenv("PORT")
+
+	if PORT == "" {
+		PORT = "7070" // Default port if not specified in .env
+	}
+
 	database.StartDB()
 	r := router.StartApp()
-	r.Run(PORT)
+	r.Run(":" + PORT)
 }
